a4243f54d2ad68049a2cad4d97ee7298c3a18b30
Victor Romero <romeroa.victor@yahoo.com>
2020-08-23 18:27:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
-#require "pry"
+require "pry"
 
 def reverse_each_word(sentence)
   new_sentence = sentence.split
 
-  new_sentence.each.reverse 
+  new_sentence.each do |words|
+    words.reverse
+    binding.pry 
+  end
+  new_sentence.join(" ")
 end
