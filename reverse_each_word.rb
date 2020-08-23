56eecb72fd2f4771aeac77bf2529db436bd6dd4d
Victor Romero <romeroa.victor@yahoo.com>
2020-08-23 18:42:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,6 @@ require "pry"
 
 def reverse_each_word(sentence)
   reverse_sentence = sentence.split
-  reverse_sentence.each.reverse
+  reverse_sentence.each do |word| reverse_sentence.reverse
+  end
 end
